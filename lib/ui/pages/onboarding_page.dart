f6301ca2205c43f7679c598b977418290c70c963
Yoga Prasetia <montirinapp@gmail.com>
2023-09-23 21:49:26 +0700

    add login

--- lib/ui/pages/onboarding_page.dart
+++ lib/ui/pages/onboarding_page.dart
@@ -1,4 +1,5 @@
 import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/pages/sign_in_page.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
@@ -129,6 +130,8 @@ class _OnboardingPageState extends State<OnboardingPage> {
                         height: 24,
                         child: TextButton(
                           onPressed: () {
+                            Navigator.push(context, MaterialPageRoute(builder: (context) => const SignInPage(),),
+                            );
                           },
                           style: TextButton.styleFrom(
                             padding: EdgeInsets.zero,
