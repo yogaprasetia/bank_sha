de682a870372c79a681dbc06f287c3104407f994
Yoga Prasetia <montirinapp@gmail.com>
2023-10-10 23:05:02 +0700

    slicing homepage profile

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -57,6 +57,72 @@ class HomePage extends StatelessWidget {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+
+        ),
+        children: [
+          buildProfile()
+        ],
+      ),
+    );
+  }
+  Widget buildProfile(){
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 40,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Howdy,',
+                style: greyTextStyle.copyWith(
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              Text(
+                'shaynahan',
+                style: blackTextStyle.copyWith(
+                  fontSize: 20,
+                  fontWeight: semiBold,
+                ),
+              )
+            ],
+          ),
+          Container(
+            width: 60,
+            height: 60,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(image: AssetImage('assets/img_profile.png'))
+            ),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                width: 16,
+                height: 16,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: whiteColor,
+                ),
+                child: Icon(
+                  Icons.check_circle,
+                  color: greenColor,
+                  size: 14,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
